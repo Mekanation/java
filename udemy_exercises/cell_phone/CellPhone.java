d225ea8941896ae2ce65b37ea4bd3048f91a2442
Caleb Waters <cw@mekanation.com>
2020-06-23 08:51:40 -0500

    Finished contact exercise

--- udemy_exercises/cell_phone/CellPhone.java
+++ udemy_exercises/cell_phone/CellPhone.java
@@ -1,8 +1,96 @@
 package udemy_exercises.cell_phone;
 
+
+import java.util.ArrayList;
+
+
 class CellPhone {
+    private String myNumber;
+    private ArrayList<Contact> Contacts;
+
+    public CellPhone(String myNumber) {
+        this.myNumber = myNumber;
+        this.Contacts = new ArrayList<Contact>();
+    }
+
+    public void showContacts(){
+        System.out.println("Contact List");
+        for(int i=0; i<this.Contacts.size(); i++){
+            System.out.println((i+1)+"."+ this.Contacts.get(i).getName() + " -> " + this.Contacts.get(i).getPhoneNumber());
+        }
+
+    }
+
+    public boolean addContact(Contact contact){
+        if(findContact(contact.getName()) > 0){
+            System.out.println("Contact is already on file");
+            return false;
+        }
+        Contacts.add(contact);
+        return true;
+
+    }
+
+    public boolean updateContact(Contact oldContact, Contact newContact){
+        int foundPosition = findContact(oldContact);
+        if(foundPosition <0){
+            System.out.println(oldContact.getName() + " was not found.");
+            return false;
+        }else if(findContact(newContact.getName()) != -1){
+            System.out.println("Contact already exists");
+            return false;
+        }
+        this.Contacts.set(foundPosition, newContact);
+        System.out.println(oldContact.getName() +" was replaced with " + newContact.getName());
+        return true;
+
+    }
+
+    public boolean removeContact(Contact contact){
+        int foundPosition = findContact(contact);
+        if(foundPosition < 0){
+            System.out.println(contact.getName() +", was not found");
+            return false;
+        }
+        this.Contacts.remove(foundPosition);
+        System.out.println("Contact was deleted");
+        return true;
+
+    }
+
+    public String searchContact(Contact contact){
+        if(findContact(contact) >=0){
+            return contact.getName();
+        }
+        return null;
+
+    }
+    public Contact searchContact(String name){
+        int position = findContact(name);
+        if(position >=0){
+            return this.Contacts.get(position);
+
+        }
+        return null;
+    }
+
+
+    private int findContact(Contact contact){
+        return this.Contacts.indexOf(contact);
+    }
+
+    private  int findContact(String contactName){
+        for(int i=0; i<this.Contacts.size(); i++){
+            Contact contact = this.Contacts.get(i);
+            if(contact.getName().equals(contactName)){
+                return i;
+            }
+        }
+        return -1;
+    }
 
 }
+
 
 /*
 **Cell Phone with Java Classes**
@@ -11,7 +99,7 @@ class CellPhone {
         - quit()
         - showContacts()
         - addContact()
-        - updateContat()
+        - updateContact()
         - removeContact()
         - searchContact()
       * **When adding or updating be sure to check if the contact already exists**
--- udemy_exercises/cell_phone/Contact.java
+++ udemy_exercises/cell_phone/Contact.java
@@ -1,7 +1,36 @@
 package udemy_exercises.cell_phone;
 
 class Contact {
+    private String name;
+    private String phoneNumber;
 
+    public Contact(String name, String phoneNumber) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+
+    public static Contact saveContact(String name, String phoneNumber){
+        return new Contact(name,phoneNumber);
+
+    }
+    public void modifyContact(){
+        // This should be done from the Cellphone Class via removing and adding a new contact
+    }
+    public void searchContact(){
+        //I dont think this belongs here.
+    }
+    public void removeContact(){
+        //I dont think this belongs here.
+    }
 }
 
 
